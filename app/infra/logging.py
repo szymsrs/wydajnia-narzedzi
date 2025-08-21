a14934edfa485733d3ec465bebdd3d867958f27b
szymsrs <ssierszulski9@gmail.com>
2025-08-21 13:35:49 +0200

    Commit z diffem z clipboardu

--- app/infra/logging.py
+++ app/infra/logging.py
@@ -1,11 +1,21 @@
 from loguru import logger
 from pathlib import Path
 import sys
+import getpass
 
-def setup_logging(log_dir: Path, level: str = "INFO"):
+
+def setup_logging(log_dir: Path, level: str = "INFO", workstation: str | None = None):
     log_dir.mkdir(parents=True, exist_ok=True)
+    fmt = "{time:YYYY-MM-DD HH:mm:ss}|{level}|{extra[os_user]}|{extra[workstation]}|{message}"
     logger.remove()
-    logger.add(sys.stdout, level=level, enqueue=True, backtrace=False, diagnose=False)
+    logger.add(
+        sys.stdout,
+        level=level,
+        enqueue=True,
+        backtrace=False,
+        diagnose=False,
+        format=fmt,
+    )
     logger.add(
         log_dir / "app.log",
         rotation="10 MB",
@@ -13,5 +23,6 @@ def setup_logging(log_dir: Path, level: str = "INFO"):
         encoding="utf-8",
         level=level,
         enqueue=True,
+        format=fmt,
     )
-    return logger
+    return logger.bind(os_user=getpass.getuser(), workstation=workstation or "UNKNOWN")
